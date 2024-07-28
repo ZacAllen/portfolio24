a126ba1b01022b3a481868b384bc5ebeec7f4ed1
Zach Allen <zallen@roomstogo.com>
2024-07-28 16:24:55 -0400

    profile card

--- src/app/components/MCard.jsx
+++ src/app/components/MCard.jsx
@@ -1,19 +1,66 @@
-import { styled, Typography, Box, Card, CardContent, CardHeader, CardMedia } from "@mui/material";
+import {
+  styled,
+  Typography,
+  Box,
+  Card,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  CardActions,
+  IconButton,
+} from '@mui/material'
 
-const Type = styled(Box)(({ theme }) => ({}));
+const StyledCard = styled(Card)(({ theme }) => ({
+  borderRadius: '5px',
+  padding: '10px',
+  backgroundColor: 'black',
+}))
+
+const StyledHeader = styled(CardHeader)(({ theme }) => ({
+  padding: '2px',
+  border: 'solid 1px black',
+  borderRadius: '5px',
+  backgroundColor: '#C4C8BC',
+  color: 'black',
+  fontFamily: theme.typography.mainFont,
+}))
+
+const Type = styled(Box)(({ theme }) => ({}))
+
+const Description = styled(Typography)(({ theme }) => ({
+  //
+}))
+const FlavorText = styled(Typography)(({ theme }) => ({
+  //
+}))
+const CardColor = styled(Box)(({ theme, background }) => ({
+  background: background,
+  padding: '10px',
+}))
 
 // Consider propTypes
 const MCard = ({ title, image, type, description, flavorText, background }) => {
   return (
-    <Card sx={{ maxWidth: "345px" }}>
-      <CardHeader title={title} />
-      <CardMedia component="img" height="194" image="" alt="z_allen" />
-      <Type>
-        <Typography>{type}</Typography>
-      </Type>
-      <CardContent></CardContent>
-    </Card>
-  );
-};
+    <StyledCard sx={{ maxWidth: '345px' }}>
+      <CardColor background={background}>
+        <StyledHeader title={title} />
+        <CardMedia component="img" height="194" image={image} alt="z_allen" />
+        <Type>
+          <Typography>{type}</Typography>
+        </Type>
+        <CardContent>
+          <Description>{description}</Description>
+          <FlavorText>{flavorText}</FlavorText>
+        </CardContent>
+      </CardColor>
 
-export default MCard;
+      <CardActions>
+        {/* Github/LinkedIn or Project Tech */}
+        <IconButton></IconButton>
+        <IconButton></IconButton>
+      </CardActions>
+    </StyledCard>
+  )
+}
+
+export default MCard
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,50 +1,52 @@
-"use client";
-import Image from "next/image";
-import { useTheme, Grid, styled, Typography, useMediaQuery, Box } from "@mui/material";
-import MCard from "./components/MCard";
-import { Padding } from "@mui/icons-material";
+'use client'
+import Image from 'next/image'
+import goku from './assets/img/goku.png'
+import { useTheme, Grid, styled, Typography, useMediaQuery, Box } from '@mui/material'
+import MCard from './components/MCard'
 
-const LandingContainer = styled("div")(({ theme }) => ({
-  height: "90vh",
-  display: "flex",
-  alignItems: "center",
-}));
+const LandingContainer = styled('div')(({ theme }) => ({
+  height: '90vh',
+  display: 'flex',
+  alignItems: 'center',
+}))
 
-const TitleContainer = styled("div")({
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "center",
-  alignItems: "baseline",
-});
+const TitleContainer = styled('div')({
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'baseline',
+})
 
 const Title = styled(Typography)(({ theme }) => ({
-  color: "white",
+  color: 'white',
   fontFamily: theme.typography.mainFont,
-  fontSize: "6rem",
-  paddingLeft: "4rem",
-  lineHeight: "0.75",
-  [theme.breakpoints.down("sm")]: {
+  fontSize: '6rem',
+  paddingLeft: '4rem',
+  lineHeight: '0.75',
+  [theme.breakpoints.down('sm')]: {
     padding: 0,
-    fontSize: "3rem",
+    fontSize: '3rem',
   },
-}));
+}))
 
 const Subtitle = styled(Typography)(({ theme }) => ({
-  color: "white",
+  color: 'white',
   fontFamily: theme.typography.mainFont,
-  fontSize: "3rem",
-  paddingLeft: "8rem",
-  [theme.breakpoints.down("sm")]: {
+  fontSize: '3rem',
+  paddingLeft: '8rem',
+  [theme.breakpoints.down('sm')]: {
     padding: 0,
-    fontSize: "1.5rem",
+    fontSize: '1.5rem',
   },
-}));
+}))
 
 const Home = () => {
   // Probably extract each subpage into their own folders later, this file will get big
-  const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
-  console.log("isMobile", isMobile);
+  const theme = useTheme()
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'))
+  // const myCardBg = `linear-gradient(to bottom, transparent, var(--background-end-rgb)) var(--background-start-rgb);`
+  const myCardBg = `linear-gradient(to bottom, transparent, #485A9A, #3A4634);`
+  console.log('isMobile', isMobile)
   return (
     <>
       {isMobile ? (
@@ -54,7 +56,7 @@ const Home = () => {
         </Box>
       ) : (
         <LandingContainer>
-          <Grid container sx={{ height: "40vh" }}>
+          <Grid container sx={{ height: '40vh' }}>
             <Grid item xs={1}></Grid>
             <Grid item xs={7}>
               <TitleContainer>
@@ -63,14 +65,14 @@ const Home = () => {
               </TitleContainer>
             </Grid>
             <Grid item xs={3}>
-              <MCard title="Zach Allen" type="Front-End Developer - Human Nerd" />
+              <MCard title="Zach Allen" image={goku} type="Front-End Developer - Human Nerd" background={myCardBg} />
             </Grid>
             <Grid item xs={1}></Grid>
           </Grid>
         </LandingContainer>
       )}
     </>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
