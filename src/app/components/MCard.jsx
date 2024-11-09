85d2e883968a02ef9c207ce3fd668386eb3f2d7b
ZacAllen <zacharysallen97@gmail.com>
2024-11-08 19:10:49 -0500

    add card animation

--- src/app/components/MCard.jsx
+++ src/app/components/MCard.jsx
@@ -9,6 +9,7 @@ import {
   CardMedia,
   CardActions,
   IconButton,
+  useMediaQuery,
 } from "@mui/material";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import Image from "next/image";
@@ -17,8 +18,9 @@ const StyledCard = styled(Card)(({ theme, isGroup }) => ({
   borderRadius: "10px",
   padding: "10px",
   backgroundColor: "black",
+  aspectRatio: "8/13",
   maxWidth: "345px",
-  minWidth: !isGroup && "300px",
+  minWidth: !isGroup ? "300px" : "240px",
   minHeight: !isGroup && "480px",
   maxHeight: !isGroup && "511px",
 }));
@@ -30,7 +32,7 @@ const StyledHeader = styled(CardHeader)(({ theme }) => ({
   backgroundColor: theme.palette.accent.medium,
 }));
 
-const StyledContent = styled(CardContent)(({ theme }) => ({
+const StyledContent = styled(CardContent)(({ theme, isGroup }) => ({
   backgroundColor: theme.palette.accent.light,
   border: "3px solid black",
   padding: "8px",
@@ -38,6 +40,9 @@ const StyledContent = styled(CardContent)(({ theme }) => ({
   position: "relative",
   bottom: "3px",
   minHeight: "170px",
+  [theme.breakpoints.down("xl")]: {
+    minHeight: isGroup && "120px",
+  },
 }));
 
 const Type = styled(Box)(({ theme }) => ({
@@ -51,13 +56,19 @@ const Type = styled(Box)(({ theme }) => ({
   // paddingLeft: "0.5rem",
 }));
 
-const Description = styled(Typography)(({ theme }) => ({
+const Description = styled(Typography)(({ theme, isGroup }) => ({
   fontFamily: theme.typography.textFont,
   fontWeight: "600",
+  [theme.breakpoints.down("xl")]: {
+    fontSize: isGroup && "12px",
+  },
 }));
-const FlavorText = styled(Typography)(({ theme }) => ({
+const FlavorText = styled(Typography)(({ theme, isGroup }) => ({
   fontFamily: theme.typography.textFont,
   fontStyle: "italic",
+  [theme.breakpoints.down("md")]: {
+    fontSize: isGroup && "12px",
+  },
 }));
 const CardColor = styled(Box)(({ theme, background }) => ({
   background: background,
@@ -70,9 +81,25 @@ const StyledIconButton = styled(IconButton)(({ theme }) => ({
 }));
 
 // Consider propTypes
+
+/**
+ * MTG-Based card component for landing and project section.
+ *
+ * @param {string} title - title of the card.
+ * @param {string} image - URL of the card image.
+ * @param {string} type - type/category of the card's content.
+ * @param {string} description - Main description text for the card.
+ * @param {string} flavorText - Additional text to provide context or details.
+ * @param {string} background - The background color or image for the card.
+ * @param {array} footerIcons - Array of icon URLs to display in the card footer.
+ * @param {string} footerText - Text to display alongside the icons in the footer.
+ * @param {boolean} isGroup - Flag indicating whether the card belongs to a group (e.g. hand animation).
+ * @returns The fully styled card component with content and actions.
+ */
 const MCard = ({ title, image, type, description, flavorText, background, footerIcons, footerText, isGroup }) => {
-  console.log(footerIcons);
   const theme = useTheme();
+  const md = useMediaQuery(theme.breakpoints.down("xl"));
+
   return (
     <StyledCard isGroup={isGroup}>
       <CardColor background={background}>
@@ -88,7 +115,7 @@ const MCard = ({ title, image, type, description, flavorText, background, footer
         <CardMedia
           sx={{ margin: "auto", maxWidth: "95%", borderRight: "3px solid black", borderLeft: "3px solid black" }}
           component="img"
-          height="194"
+          height={md && isGroup ? "124" : "194"}
           image={image}
           alt="card image"
         />
@@ -98,9 +125,9 @@ const MCard = ({ title, image, type, description, flavorText, background, footer
             <AccountCircleIcon />
           </StyledIconButton>
         </Type>
-        <StyledContent>
-          <Description>{description}</Description>
-          <FlavorText>{flavorText}</FlavorText>
+        <StyledContent isGroup={isGroup}>
+          <Description isGroup={isGroup}>{description}</Description>
+          <FlavorText isGroup={isGroup}>{flavorText}</FlavorText>
         </StyledContent>
       </CardColor>
 
