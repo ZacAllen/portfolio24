bb4c73cba4d2fc14cd5573d34e046e6bf14a1eb8
ZacAllen <zacharysallen97@gmail.com>
2024-12-09 02:52:42 -0500

    Update Tools.jsx

--- src/app/subpages/Tools.jsx
+++ src/app/subpages/Tools.jsx
@@ -1,10 +1,10 @@
-'use client'
-import { useTheme, styled, Typography } from '@mui/material'
-import { DarkModeContext } from '@/utils/helpers/DarkModeContext'
-import { useContext, useState } from 'react'
-import { motion, useAnimate } from 'motion/react'
-import Image from 'next/image'
-import { Swiper, SwiperSlide } from 'swiper/react'
+"use client";
+import { useTheme, styled, Typography } from "@mui/material";
+import { DarkModeContext } from "@/utils/helpers/DarkModeContext";
+import { useContext, useState } from "react";
+import { AnimatePresence, motion, useAnimate } from "motion/react";
+import Image from "next/image";
+import { Swiper, SwiperSlide } from "swiper/react";
 import {
   JavaScript,
   Java,
@@ -23,48 +23,47 @@ import {
   Git,
   Growthbook,
   HTML5,
-} from '../../utils/helpers/skillsConfig'
-import { Autoplay, EffectFade } from 'swiper/modules'
+} from "../../utils/helpers/skillsConfig";
+import { Autoplay, EffectFade } from "swiper/modules";
+
+const imgArray = {
+  languages: [
+    { name: "JavaScript", url: JavaScript },
+    { name: "TypeScript", url: TypeScript },
+    { name: "HTML5", url: HTML5 },
+    { name: "CSS3", url: CSS3 },
+    { name: "Java", url: Java },
+    { name: "Processing", url: Processing },
+    { name: "p5js", url: p5js },
+  ],
+  tools: [
+    { name: "git", url: Git },
+    { name: "React", url: ReactImg },
+    { name: "NextJS", url: NextJS },
+    { name: "Node", url: Node },
+    { name: "TailwindCSS", url: Tailwind },
+    { name: "Bootstrap", url: Bootstrap },
+    { name: "Gatsby", url: Gatsby },
+    { name: "Material UI", url: Mui },
+    { name: "Jira", url: Jira },
+    { name: "Growthbook", url: Growthbook },
+  ],
+};
 
 const Tools = ({ isMobile }) => {
-  const { isDarkMode, textColor, lighttext, darktext } = useContext(DarkModeContext)
+  const { isDarkMode, textColor, lighttext, darktext } = useContext(DarkModeContext);
 
-  const SliderImage = styled(Image)({})
+  const SliderImage = styled(Image)({});
 
   const SlideText = styled(Typography)(({ theme }) => ({
     fontFamily: theme.typography.textFont,
-    fontSize: '1.5rem',
-    marginTop: '1rem',
+    fontSize: "1.5rem",
+    marginTop: "1rem",
     color: textColor,
-  }))
+  }));
 
   const createSlides = (key) => {
-    let imgArray = []
-    if (key === 'languages') {
-      imgArray = [
-        { name: 'JavaScript', url: JavaScript },
-        { name: 'TypeScript', url: TypeScript },
-        { name: 'HTML5', url: HTML5 },
-        { name: 'CSS3', url: CSS3 },
-        { name: 'Java', url: Java },
-        { name: 'Processing', url: Processing },
-        { name: 'p5js', url: p5js },
-      ]
-    } else if (key === 'frameworks') {
-      imgArray = [
-        { name: 'git', url: Git },
-        { name: 'React', url: ReactImg },
-        { name: 'NextJS', url: NextJS },
-        { name: 'Node', url: Node },
-        { name: 'TailwindCSS', url: Tailwind },
-        { name: 'Bootstrap', url: Bootstrap },
-        { name: 'Gatsby', url: Gatsby },
-        { name: 'Material UI', url: Mui },
-        { name: 'Jira', url: Jira },
-        { name: 'Growthbook', url: Growthbook },
-      ]
-    }
-    return imgArray.map((img) => (
+    return imgArray?.[key].map((img) => (
       <SwiperSlide className="!flex flex-col justify-center items-center">
         <SliderImage
           src={img.url}
@@ -72,44 +71,75 @@ const Tools = ({ isMobile }) => {
           width={0}
           alt=""
           style={{
-            maxWidth: '80%',
-            maxHeight: '75px',
-            width: 'auto',
+            maxWidth: "80%",
+            maxHeight: "75px",
+            width: "auto",
           }}
         ></SliderImage>
         <SlideText>{img.name}</SlideText>
       </SwiperSlide>
-    ))
-  }
+    ));
+  };
+
+  const createList = (key) => {
+    console.log("*** Hello", key);
+    return (
+      <div className="yo">
+        {imgArray?.[key].map((img) => (
+          <div className="flex items-center flex-row">
+            <div>
+              <Image width={50} src={img?.url} />
+            </div>
+            <div>{img?.name}</div>
+          </div>
+        ))}
+      </div>
+    );
+  };
 
   const ToolsTitle = styled(Typography)(({ theme }) => ({
     fontFamily: theme.typography.mainFont,
-    fontSize: '4rem',
+    fontSize: "4rem",
     color: textColor,
-  }))
-  const Tooltip = styled('p')(({ theme }) => ({
+  }));
+  const Tooltip = styled("p")(({ theme }) => ({
     fontFamily: theme.typography.mainFont,
     color: textColor,
-    fontSize: '1.5rem',
-    [theme.breakpoints.down('sm')]: {
-      fontSize: '1rem',
+    fontSize: "1.5rem",
+    [theme.breakpoints.down("sm")]: {
+      fontSize: "1rem",
       margin: 0,
     },
-    margin: '0 0 0 2rem',
-  }))
+    margin: "0 0 0 2rem",
+  }));
   const Languages = styled(Typography)(({ theme }) => ({
     fontFamily: theme.typography.mainFont,
     color: textColor,
-    fontSize: '2rem',
-    [theme.breakpoints.down('sm')]: {
-      fontSize: '1.5rem',
+    fontSize: "2rem",
+    [theme.breakpoints.down("sm")]: {
+      fontSize: "1.5rem",
     },
-    cursor: 'pointer',
-  }))
+    cursor: "pointer",
+  }));
 
   const StyledSwiper = styled(Swiper)(({ theme }) => ({
     maskImage: `linear-gradient(90deg, transparent 1%, black 5%, black 95%, transparent 99%)`,
-  }))
+  }));
+
+  const [languagesScope, langaugesAnim] = useAnimate();
+  const [toolsScope, toolsAnim] = useAnimate();
+  const [showSliderL, setShowSliderL] = useState(true);
+  const [showSliderA, setShowSliderA] = useState(true);
+
+  const handleToolsList = (e) => {
+    if (e.target.id === "lang") {
+      langaugesAnim(languagesScope.current, {}, { duration: 0.5 });
+      setShowSliderL(false); //!add timer before setting state
+    } else if (e.target.id === "tool") {
+      toolsAnim(toolsScope.current, {}, { duration: 0.5 });
+      setShowSliderA(false);
+    }
+  };
 
   return (
     <div className="my-32">
@@ -118,52 +148,72 @@ const Tools = ({ isMobile }) => {
         <Tooltip className="">* Click for list view</Tooltip>
       </div>
       <div className="mb-2">
-        <Languages>Programming Languages *</Languages>
+        <Languages id="lang" onClick={(e) => handleToolsList(e)}>
+          Programming Languages *
+        </Languages>
       </div>
       <div className="relative mb-16">
-        <motion.div>
-          <StyledSwiper
-            className="swiper-transition"
-            freeMode={true}
-            slidesPerView={isMobile ? 3 : 6}
-            direction="horizontal"
-            modules={[Autoplay]}
-            autoplay={{
-              delay: 0,
-              disableOnInteraction: false, //! May have to change this for accessibility
-            }}
-            speed={1800}
-            loop={true}
-          >
-            {createSlides('languages')}
-          </StyledSwiper>
-        </motion.div>
+        <AnimatePresence>
+          {showSliderL ? (
+            <motion.div ref={languagesScope} exit={{ opacity: 0 }}>
+              <StyledSwiper
+                className="swiper-transition"
+                freeMode={true}
+                slidesPerView={isMobile ? 3 : 6}
+                direction="horizontal"
+                modules={[Autoplay]}
+                autoplay={{
+                  delay: 0,
+                  disableOnInteraction: false, //! May have to change this for accessibility
+                }}
+                speed={1800}
+                loop={true}
+              >
+                {createSlides("languages")}
+              </StyledSwiper>
+            </motion.div>
+          ) : (
+            <motion.div>
+              <div>{createList("languages")}</div>
+            </motion.div>
+          )}
+        </AnimatePresence>
       </div>
       <div className="mb-2">
-        <Languages>Tools & Frameworks *</Languages>
+        <Languages id="tool" onClick={(e) => handleToolsList(e)}>
+          Tools & Frameworks *
+        </Languages>
       </div>
       <div className="relative mb-16">
-        <motion.div>
-          <StyledSwiper
-            className="swiper-transition"
-            freeMode={true}
-            slidesPerView={isMobile ? 3 : 7}
-            direction="horizontal"
-            modules={[Autoplay]}
-            autoplay={{
-              delay: 0,
-              reverseDirection: true,
-              disableOnInteraction: false, //! May have to change this for accessibility
-            }}
-            speed={1800}
-            loop={true}
-          >
-            {createSlides('frameworks')}
-          </StyledSwiper>
-        </motion.div>
+        <AnimatePresence>
+          {showSliderA ? (
+            <motion.div ref={toolsScope}>
+              <StyledSwiper
+                className="swiper-transition"
+                freeMode={true}
+                slidesPerView={isMobile ? 3 : 7}
+                direction="horizontal"
+                modules={[Autoplay]}
+                autoplay={{
+                  delay: 0,
+                  reverseDirection: true,
+                  disableOnInteraction: false, //! May have to change this for accessibility
+                }}
+                speed={1800}
+                loop={true}
+              >
+                {createSlides("tools")}
+              </StyledSwiper>
+            </motion.div>
+          ) : (
+            <motion.div>
+              <div>{createList("tools")}</div>
+            </motion.div>
+          )}
+        </AnimatePresence>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Tools
+export default Tools;
