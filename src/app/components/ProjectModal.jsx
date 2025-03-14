7a0a7d75399890a8cc6e20903cd795bb7a35bc3a
Zach Allen <zallen@roomstogo.com>
2025-03-14 17:17:00 -0400

    Update ProjectModal.jsx

--- src/app/components/ProjectModal.jsx
+++ src/app/components/ProjectModal.jsx
@@ -1,50 +1,54 @@
-"use client";
-import React, { useState, useEffect, useContext } from "react";
-import { motion, useAnimate } from "motion/react";
-import { Modal, Typography, Button, styled, CircularProgress, IconButton } from "@mui/material";
-import CloseIcon from "@mui/icons-material/Close";
-import { projects } from "@/utils/helpers/projectsConfig";
-import { DarkModeContext } from "@/utils/helpers/DarkModeContext";
+'use client'
+import React, { useState, useEffect, useContext } from 'react'
+import { motion, useAnimate } from 'motion/react'
+import { Modal, Typography, Button, styled, CircularProgress, IconButton } from '@mui/material'
+import CloseIcon from '@mui/icons-material/Close'
+import { projects } from '@/utils/helpers/projectsConfig'
+import { DarkModeContext } from '@/utils/helpers/DarkModeContext'
 
 const StyledModal = styled(Modal)(({ theme }) => ({
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-}));
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}))
 
-const StyledContainer = styled("div")(({ theme, bgColor }) => ({
-  backgroundColor: bgColor,
-}));
+const StyledContainer = styled('div')(({ theme, bgColor }) => ({
+  background: `${bgColor}`,
+}))
 
 const CloseButton = styled(IconButton)({
-  position: "absolute",
+  position: 'absolute',
   top: 0,
   right: 0,
-});
+})
 
-const Title = styled(Typography)(({ theme }) => {});
+const Title = styled(Typography)(({ theme }) => ({
+  fontFamily: theme.typography.mainFont,
+  fontSize: '3rem',
+  color: 'white',
+}))
 
 const ProjectModal = ({ open, setOpen, projectName }) => {
-  const [scope, animate] = useAnimate();
-  const [project, setProject] = useState({});
-  const { background } = useContext(DarkModeContext);
-  console.log("*** BFG", background);
+  const [scope, animate] = useAnimate()
+  const [project, setProject] = useState({})
+  const { background } = useContext(DarkModeContext)
+  console.log('*** BFG', background)
 
   useEffect(() => {
-    setProject(projects.find((proj) => proj.name === projectName));
-  }, [projectName]);
+    setProject(projects.find((proj) => proj.name === projectName))
+  }, [projectName])
 
-  console.log("*** Project", project, projectName);
+  console.log('*** Project', project, projectName)
 
   useEffect(() => {
     // animate(scope.current, { translateY: "100vh" }, { ease: "linear", duration: 0.2 });
-  }, [open]);
+  }, [open])
 
   const closeModal = () => {
     // animate(scope.current, { translateY: "0vh" }, { ease: "linear", duration: 0.2 }).then(() => {
-    setOpen(false);
+    setOpen(false)
     // });
-  };
+  }
 
   return (
     <>
@@ -52,9 +56,10 @@ const ProjectModal = ({ open, setOpen, projectName }) => {
         <motion.div ref={scope}>
           <StyledContainer
             className="flex justify-center items-center relative rounded-xl p-4 lg:w-[60rem] md:w-[40rem] sm:w-[20rem] w-[16rem]"
-            bgColor={background}
+            // bgColor={background}
+            bgColor={project?.backgroundColor}
           >
-            <div className="">
+            <div className="w-full text-left">
               <Title>{project?.name}</Title>
             </div>
             <div className="grid grid-cols-4 gap-4">
@@ -62,13 +67,13 @@ const ProjectModal = ({ open, setOpen, projectName }) => {
               <div className="col-span-2"></div>
             </div>
             <CloseButton onClick={() => closeModal()}>
-              <CloseIcon sx={{ color: "gray" }} />
+              <CloseIcon sx={{ color: 'gray' }} />
             </CloseButton>
           </StyledContainer>
         </motion.div>
       </StyledModal>
     </>
-  );
-};
+  )
+}
 
-export default ProjectModal;
+export default ProjectModal
