31bd63b11d91a6e7787e951fc9873dc94535fe30
ZacAllen2 <34634716+ZacAllen@users.noreply.github.com>
2025-03-21 19:25:28 -0400

    Update create-env.js

--- create-env.js
+++ create-env.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const path = `./.env`;
 const vars = `
 NEXT_PUBLIC_EMAILJS_SERVICE_ID=${process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID}\n
-NEXT_PUBLIC_EMAILJS_TEMPLATE_IDD=${process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID}\n
+NEXT_PUBLIC_EMAILJS_TEMPLATE_ID=${process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID}\n
 NEXT_PUBLIC_EMAIL_JS_USER_ID=${process.env.NEXT_PUBLIC_EMAIL_JS_USER_ID}
 `;
 fs.writeFileSync(path, vars);
