981a5ef8f1f0bd76eab10d73e6a4ee8e0b6ca059
Zach Allen <zallen@roomstogo.com>
2025-03-22 19:38:44 -0400

    fix mobile project modal focus bug

--- src/app/components/MCard.jsx
+++ src/app/components/MCard.jsx
@@ -10,91 +10,92 @@ import {
   CardActions,
   IconButton,
   useMediaQuery,
-} from "@mui/material";
-import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-import Image from "next/image";
+} from '@mui/material'
+import AccountCircleIcon from '@mui/icons-material/AccountCircle'
+import Image from 'next/image'
+import { useMobile } from '@/utils/helpers/MobileContext'
 
 const StyledCard = styled(Card, {
-  shouldForwardProp: (prop) => prop !== "isGroup",
+  shouldForwardProp: (prop) => prop !== 'isGroup',
 })(({ theme, isGroup }) => ({
-  borderRadius: "10px",
-  padding: "10px",
-  backgroundColor: "black",
-  aspectRatio: "8/13",
-  maxWidth: "345px",
-  minWidth: isGroup ? "240px" : "300px",
-  minHeight: isGroup ? null : "480px",
-  maxHeight: isGroup ? "450px" : "511px",
-  [theme.breakpoints.down("md")]: {
-    maxHeight: isGroup ? "392px" : null,
+  borderRadius: '10px',
+  padding: '10px',
+  backgroundColor: 'black',
+  aspectRatio: '8/13',
+  maxWidth: '345px',
+  minWidth: isGroup ? '240px' : '300px',
+  minHeight: isGroup ? null : '480px',
+  maxHeight: isGroup ? '450px' : '511px',
+  [theme.breakpoints.down('md')]: {
+    maxHeight: isGroup ? '392px' : null,
   },
-}));
+}))
 
 const StyledHeader = styled(CardHeader)(({ theme }) => ({
-  padding: "2px",
-  border: "solid 3px black",
-  borderRadius: "10px",
+  padding: '2px',
+  border: 'solid 3px black',
+  borderRadius: '10px',
   backgroundColor: theme.palette.accent.medium,
-}));
+}))
 
 const StyledContent = styled(CardContent, {
-  shouldForwardProp: (prop) => prop !== "isGroup",
+  shouldForwardProp: (prop) => prop !== 'isGroup',
 })(({ theme, isGroup }) => ({
   backgroundColor: theme.palette.accent.light,
-  border: "3px solid black",
-  padding: "8px",
-  margin: "0 5px -24px 5px",
-  position: "relative",
-  bottom: "3px",
-  minHeight: "7.625rem",
-  [theme.breakpoints.down("xl")]: {
-    minHeight: isGroup && "8rem",
+  border: '3px solid black',
+  padding: '8px',
+  margin: '0 5px -24px 5px',
+  position: 'relative',
+  bottom: '3px',
+  minHeight: '7.625rem',
+  [theme.breakpoints.down('xl')]: {
+    minHeight: isGroup && '8rem',
   },
-}));
+}))
 
 const Type = styled(Box)(({ theme }) => ({
-  display: "flex",
-  flexDirection: "row",
-  justifyContent: "space-between",
-  padding: "0 5px 0 15px",
-  border: "solid 3px black",
-  borderRadius: "10px",
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  padding: '0 5px 0 15px',
+  border: 'solid 3px black',
+  borderRadius: '10px',
   backgroundColor: theme.palette.accent.medium,
   // paddingLeft: "0.5rem",
-}));
+}))
 
 const Description = styled(Typography, {
-  shouldForwardProp: (prop) => prop !== "isGroup",
+  shouldForwardProp: (prop) => prop !== 'isGroup',
 })(({ theme, isGroup }) => ({
   fontFamily: theme.typography.textFont,
-  fontWeight: "600",
-  [theme.breakpoints.down("xl")]: {
-    fontSize: isGroup && "12px",
+  fontWeight: '600',
+  [theme.breakpoints.down('xl')]: {
+    fontSize: isGroup && '12px',
   },
-}));
+}))
 
 const FlavorText = styled(Typography, {
-  shouldForwardProp: (prop) => prop !== "isGroup",
+  shouldForwardProp: (prop) => prop !== 'isGroup',
 })(({ theme, isGroup }) => ({
   fontFamily: theme.typography.textFont,
-  fontStyle: "italic",
-  [theme.breakpoints.down("md")]: {
-    fontSize: isGroup && "12px",
+  fontStyle: 'italic',
+  [theme.breakpoints.down('md')]: {
+    fontSize: isGroup && '12px',
   },
-}));
+}))
 
 const CardColor = styled(Box, {
-  shouldForwardProp: (prop) => prop !== "background",
+  shouldForwardProp: (prop) => prop !== 'background',
 })(({ theme, background }) => ({
   background: background,
-  padding: "5px",
-  borderRadius: "10px",
-}));
+  padding: '5px',
+  borderRadius: '10px',
+}))
 
 const StyledIconButton = styled(IconButton)(({ theme }) => ({
   padding: 0,
-  marginRight: "15px",
-}));
+  marginRight: '15px',
+}))
 
 // Consider propTypes
 
@@ -127,31 +128,38 @@ const MCard = ({
   isGroup,
   setCurrentCard,
 }) => {
-  const theme = useTheme();
-  const md = useMediaQuery(theme.breakpoints.down("xl"));
+  const theme = useTheme()
+  const md = useMediaQuery(theme.breakpoints.down('xl'))
+  const isMobile = useMobile()
+
+  const setCurrentMobile = (title) => {
+    if (isMobile && setCurrentCard) {
+      setCurrentCard(title)
+    }
+  }
 
   return (
-    <div id={id} onMouseEnter={() => setCurrentCard && setCurrentCard(title)}>
+    <div id={id} onMouseEnter={() => setCurrentCard && setCurrentCard(title)} onClick={() => setCurrentMobile(title)}>
       <StyledCard isGroup={isGroup}>
         <CardColor background={background}>
           <StyledHeader
             title={title}
             titleTypographyProps={{
-              color: "black",
+              color: 'black',
               fontFamily: theme.typography.mainFont,
-              paddingLeft: "0.5rem",
-              fontSize: "1rem",
+              paddingLeft: '0.5rem',
+              fontSize: '1rem',
             }}
           />
           <CardMedia
-            sx={{ margin: "auto", maxWidth: "95%", borderRight: "3px solid black", borderLeft: "3px solid black" }}
+            sx={{ margin: 'auto', maxWidth: '95%', borderRight: '3px solid black', borderLeft: '3px solid black' }}
             component="img"
-            height={md && isGroup ? "124" : "194"}
+            height={md && isGroup ? '124' : '194'}
             image={image}
             alt="card image"
           />
           <Type>
-            <Typography sx={{ color: "black", fontFamily: theme.typography.mainFont }}>{type}</Typography>
+            <Typography sx={{ color: 'black', fontFamily: theme.typography.mainFont }}>{type}</Typography>
             <StyledIconButton size="large" edge="end" color="inherit">
               <AccountCircleIcon />
             </StyledIconButton>
@@ -162,7 +170,7 @@ const MCard = ({
           </StyledContent>
         </CardColor>
 
-        <CardActions sx={{ padding: "20px 15px 0 15px", display: "flex", justifyContent: "space-between" }}>
+        <CardActions sx={{ padding: '20px 15px 0 15px', display: 'flex', justifyContent: 'space-between' }}>
           {/* Github/LinkedIn or Project Tech */}
           <div>
             {footerIcons.map((icon) => (
@@ -172,13 +180,13 @@ const MCard = ({
             ))}
           </div>
 
-          <Typography color={"white"} sx={{ fontFamily: theme.typography.textFont }}>
+          <Typography color={'white'} sx={{ fontFamily: theme.typography.textFont }}>
             {footerText}
           </Typography>
         </CardActions>
       </StyledCard>
     </div>
-  );
-};
+  )
+}
 
-export default MCard;
+export default MCard
